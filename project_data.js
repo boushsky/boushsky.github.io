9eda1f3d80e4d2c0aa3427a637803d683f23e826
Xuchen Wang <xwang246@uic.edu>
2017-11-14 06:24:18 +0000

    portfolio update

---
+++ project_data.js
@@ -0,0 +1,37 @@
+var projects = [
+	{"name": "Project1", 
+	"display": false, 
+	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"linkToProject":"https://boushsky.github.io/IT202/project1/"
+	},
+	{"name": "Project2", 
+	"display": false, 
+	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"linkToProject":"https://boushsky.github.io/IT202/project2/"
+	},
+	{"name": "Project3", 
+	"display": false, 
+	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"linkToProject":"https://boushsky.github.io/IT202/project3/"
+	},
+	{"name": "Project4", 
+	"display": false, 
+	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"linkToProject":"https://boushsky.github.io/IT202/project4/"
+	},
+	{"name": "Project5", 
+	"display": false, 
+	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"linkToProject":"https://boushsky.github.io/IT202/project5/"
+	},
+	{"name": "Project7", 
+	"display": false, 
+	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"linkToProject":"https://boushsky.github.io/IT202/project7/"
+	},
+	
+	
+	
+
+
+]
