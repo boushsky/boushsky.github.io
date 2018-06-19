9bcb5e6e06df497d917617470a6797e3167f4c47
Xuchen Wang <xwang246@uic.edu>
2018-06-19 00:10:27 +0000

    info update 6/18/2018

--- project_data.js
+++ project_data.js
@@ -1,32 +1,39 @@
 var projects = [
 	{"name": "Project1", 
+	"project_name":"",
 	"display": false, 
-	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
 	"linkToProject":"https://boushsky.github.io/IT202/project1/"
+	
 	},
 	{"name": "Project2", 
+	"project_name":"",
 	"display": false, 
-	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
 	"linkToProject":"https://boushsky.github.io/IT202/project2/"
 	},
-	{"name": "Project3", 
+	{"name": "Project3",
+	"project_name":"",
 	"display": false, 
-	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
 	"linkToProject":"https://boushsky.github.io/IT202/project3/"
 	},
-	{"name": "Project4", 
+	{"name": "Project4",
+	"project_name":"",
 	"display": false, 
-	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
 	"linkToProject":"https://boushsky.github.io/IT202/project4/"
 	},
 	{"name": "Project5", 
+	"project_name":"",
 	"display": false, 
-	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
 	"linkToProject":"https://boushsky.github.io/IT202/project5/"
 	},
 	{"name": "Project7", 
+	"project_name":"",
 	"display": false, 
-	"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
 	"linkToProject":"https://boushsky.github.io/IT202/project7/"
 	},
 	
