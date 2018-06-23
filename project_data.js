2d06d41e69917543dc47c4d2ac0ab3726a640a26
Xuchen Wang <xwang246@uic.edu>
2018-06-23 11:54:18 +0000

    update 6_23_18

--- project_data.js
+++ project_data.js
@@ -1,42 +1,59 @@
 var projects = [
-	{"name": "Project1", 
+	{"name": "My favorite milk", 
 	"project_name":"",
 	"display": false, 
-	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"descript" : "My personal Top 3 milk brand.",
 	"linkToProject":"https://boushsky.github.io/IT202/project1/"
 	
 	},
-	{"name": "Project2", 
+	{"name": "Basic Fitness website", 
 	"project_name":"",
 	"display": false, 
-	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"descript" : "Create a simple fitness website using exist template.",
 	"linkToProject":"https://boushsky.github.io/IT202/project2/"
 	},
-	{"name": "Project3",
+	{"name": "Food Inspection",
 	"project_name":"",
 	"display": false, 
-	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"descript" : "A really cool food inspection project using API sort information from website.",
 	"linkToProject":"https://boushsky.github.io/IT202/project3/"
 	},
-	{"name": "Project4",
+	{"name": "Ball webgame",
 	"project_name":"",
 	"display": false, 
 	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
 	"linkToProject":"https://boushsky.github.io/IT202/project4/"
 	},
-	{"name": "Project5", 
+	{"name": "Weather forecast", 
 	"project_name":"",
 	"display": false, 
-	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"descript" : "A really cool weather forecast as my first progrssion web app",
 	"linkToProject":"https://boushsky.github.io/IT202/project5/"
 	},
-	{"name": "Project7", 
+	{"name": "Sound horn", 
 	"project_name":"",
 	"display": false, 
-	"descript" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.",
+	"descript" : "A really cool sound horn project using AJAX",
 	"linkToProject":"https://boushsky.github.io/IT202/project7/"
 	},
-	
+	{"name": "Tic Tac Toe", 
+	"project_name":"",
+	"display": false, 
+	"descript" : "A really Tic Tac Toe project using AJAX, React to play with",
+	"linkToProject":"https://boushsky.github.io/IT202/project8/"
+	},
+	{"name": "Friendly Chat", 
+	"project_name":"",
+	"display": false, 
+	"descript" : "A really cool Chat project using Firebase Web",
+	"linkToProject":"https://boushsky.github.io/IT202/project9/"
+	},
+	{"name": "Push Notification", 
+	"project_name":"",
+	"display": false, 
+	"descript" : "A really push notification project using Push API.",
+	"linkToProject":"https://boushsky.github.io/IT202/project10/"
+	},
 	
 	
 
